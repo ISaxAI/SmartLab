4726cd0fdace622a89f00b33511021e953951dd8
rmp <asdasdasd>
2023-05-02 16:35:51 +0300

    new commit

--- app/src/main/java/com/example/smartlab/MainActivity.java
+++ app/src/main/java/com/example/smartlab/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.smartlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(MainActivity.this, Onboard1.class);
+                startActivity(i);
+                // close this activity
+                finish();
+            }
+        }, 3000);
     }
+
+
 }
